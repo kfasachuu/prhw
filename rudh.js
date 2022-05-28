05e2e5c17681c025e542c9ce4517aafad632e94e
KFASACHU <89623675+KFASACHU@users.noreply.github.com>
2022-05-28 10:47:07 +0300

    Update rudh.js

--- rudh.js
+++ rudh.js
@@ -2,7 +2,7 @@
 /**
    * MIT License
 * 
-   * Copyright (c) 2021 prince-rudh.
+   * Copyright (c) 2022 prince-rudh.
 * 
    * Permission is hereby granted, free of charge, to any person obtaining a copy
    * of this software and associated documentation files (the "Software"), to deal
